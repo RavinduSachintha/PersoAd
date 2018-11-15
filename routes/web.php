3911b3dff6432b5da7b20a50b81e05a054e137c7
Sachintha <ravindusachintha53@gmail.com>
2018-11-15 17:15:37 +0530

    update initialized files

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/product-details', function () {
+    return view('product-details');
 });
