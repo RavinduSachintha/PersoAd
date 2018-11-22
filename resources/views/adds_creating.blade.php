958395ce8da7a6a2c7f696fa971f3fb4c4fa5a21
Nidu Sihani <36721132+Nidu96@users.noreply.github.com>
2018-11-22 12:40:46 +0530

    Advertisemnts creating
    Only the back end has done. UI s ntot yet desinged

---
+++ resources/views/adds_creating.blade.php
@@ -0,0 +1,39 @@
+
+            <div class="container border text-center" style="border-radius:10px;padding: 0px; margin-bottom: 20px">
+
+                <h4 class="cardheader" style="color: white">{{ __('Create Notifications') }}</h4>
+                <div class="container">
+
+                    <form method="POST" action="/adds_creating">
+                      {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <label for="title"><b>{{ __('Title') }}</b></label>
+                                </div>
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control" id="title" name="title" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <label for="body"><b>{{ __('Notification') }}</b></label>
+                                </div>
+                                <div class="col-md-10">
+                                    <textarea id="body" name="body" class="form-control" required></textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+
+                    </form>
+                    </div>
+
+                </div>
+            
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/adds_creating', 'AdvertisementsController@create');
+Route::post('/adds_creating', 'AdvertisementsController@store');
